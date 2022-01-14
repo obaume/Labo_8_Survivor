bb18545f73bf32e22b13a0121eb00b5f6f31b127
SirDek <cedric.centeno@gmail.com>
2022-01-14 21:03:38 +0100

    Robotv2

--- cpp/Robot.cpp
+++ cpp/Robot.cpp
@@ -9,6 +9,12 @@ Data Robot::total    = 0;
 
 Robot::Robot():NO_(suivant) {
    coord.first = coord.second = 0;
+   ++total;
+   ++suivant;
+}
+
+Robot::~Robot() {
+   --total
 }
 
 Robot::Robot(int coord) {}
@@ -19,7 +25,11 @@ bool Robot::operator==(const Robot& r) {
  return (coord == r.coord);
 }
 
-   void Robot::deplacer(Direction direction) {
+void Robot::deplacer(Coord coord) {
+   this->coord = coord;
+}
+
+void Robot::deplacer(Direction direction) {
    switch (direction) {
          case Direction::HAUT   : coord.second += 1; break;
 
@@ -29,6 +39,10 @@ bool Robot::operator==(const Robot& r) {
 
          case Direction::DROITE : coord.first  += 1; break;
       }
+}
+
+   Data Robot::get_num(const Robot& r) {
+   return
 }
 
 }
